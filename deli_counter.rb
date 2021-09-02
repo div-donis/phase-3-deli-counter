89d9ba797f8f7990181e96676d18fb7d0247099d
Donovan <divdonis@gmail.com>
2021-09-01 21:31:35 -0700

    commit tree 3c6293a3b42fd485f3f7124c20fb3074ed8e1b66

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+    if katz_deli.length == 0
+        puts "The line is currently empty."
+    else
+        persons = katz_deli.to_enum.with_index(1).map do |name, index| 
+        "#{index}. #{name}"
+        end        
+        puts "The line is currently: #{persons.join(" ")}"
+    end
+end
+
+def take_a_number(katz_deli, guppy)
+    katz_deli << guppy 
+    puts "Welcome, #{guppy}. You are number #{katz_deli.index(guppy) + 1} in line."
+end 
+def now_serving(katz_deli)
+    if katz_deli.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{katz_deli[0]}."
+        katz_deli.shift
+    end
+end
